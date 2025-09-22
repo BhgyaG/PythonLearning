099a5957fc3169eded50a9f90c91fcfd7a2659f2
BhgyaG <bhagyasuryamounikaganisetti@gmail.com>
2025-09-21 20:36:10 -0400

    Delete grade_calculator.py

--- grade_calculator.py
+++
@@ -1,9 +0,0 @@
-marks=int(input("enter marks:"))
-if marks>=90:
-    print("grade 1")
-elif marks>=75:
-    print("grade 2")
-elif marks>=50 and marks<75: #we dont need to write marks<75 here in this line i just wrote for my undersatanding
-    print("grade 3")
-else:
-    print("fail")
